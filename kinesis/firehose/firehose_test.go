0bc388ae8265e90f8bab073a1e117cf982c824f7
Bhavik Kumar <bhavikk85@gmail.com>
2019-07-19 17:48:24 +1200

    Add test for processing records

--- kinesis/firehose/firehose_test.go
+++ kinesis/firehose/firehose_test.go
@@ -1,6 +1,7 @@
 package firehose_test
 
 import (
+	"encoding/base64"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/bhavikkumar/kinesis-firehose-cloudwatch-log-processor/kinesis/firehose"
 	"github.com/stretchr/testify/assert"
@@ -53,4 +54,22 @@ func TestCreateReingestData(t *testing.T) {
 		reingestData := firehose.CreateReingestData(test.record, test.isSourceAStream)
 		assert.Equal(t, test.reingestData, reingestData)
 	}
+}
+
+func TestProcessRecords(t *testing.T) {
+	payload, err := base64.StdEncoding.DecodeString("H4sIAAAAAAAAAJWRTWsbMRCG/8ueLZjRjL5yc9NNLnZDapemlFAkrTYstb3Lep0Qgv97x00KgTSHnAQzmkeP3nmqtmW/j3dl/TiU6qz6PF/Pfy3r1Wp+WVezqn/YlVHK2pK3Hr0Jxkt5099djv1hkE7uh0eVHzZqE7epiarb3fe/ixzDYVJoELRhssYQqsXLlEJ3jd8//biy4QYWz7jVNJa4/TDveQwV+qsada0v/HnthLg/pH0eu2Hq+t1Ft5nKuK/Ofn4EvnpDUAu7Xi6/LL9en3/z1e1f7fq+7KYT+qnqGrEnsi54AGS2wbHWxjCjoWAYGawmzawByIG3Dp0JzjOxsaI8dbKJKW4l1BcTdgg+zP5tSPCeQ/Bso/I+o+I2kUptjgrRlQyasslUHWdvZRwGJ4+HYJGCtiKgQTYKSJ4gODLgAkpFk3f0rkyA1zLGSsvoVsVCRTFakUkNqKxt1IyFc8T/y0gEmoHZo5a/W9HhU0TeWHMyIJaoQC6zDvC+DL6WSW3MqZSkiolJcWoalWybJSNIJTXcRgjV8fb4BwwLrNzwAgAA")
+	record := events.KinesisFirehoseEventRecord{
+		Data:     payload,
+		RecordID: "49578734086442259037497492980620233840400173390482112514000000",
+	}
+
+	var records []events.KinesisFirehoseEventRecord
+	records = append(records, record)
+
+	responseRecords, reingestRecords := firehose.ProcessRecords(records, true)
+
+	assert.NoError(t, err)
+	assert.Len(t, responseRecords, 1)
+	assert.Len(t, reingestRecords, 1)
+	assert.Equal(t, reingestRecords[0].Data, record.Data)
 }
